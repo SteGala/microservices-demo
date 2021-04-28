4f35f1f20886b758fb79c3617897de5f532b1aee
SteGala <galaxx.ste@gmail.com>
2021-04-28 15:35:13 +0200

    Updated Loadgenerator

--- src/loadgenerator/locustfile.py
+++ src/loadgenerator/locustfile.py
@@ -16,73 +16,111 @@
 
 import random
 import math
-from locust import HttpUser, TaskSet, between, stats
+from locust import HttpUser, TaskSet, between, stats, task
 from locust import LoadTestShape
+from locust.contrib.fasthttp import FastHttpUser
+
 
 stats.CSV_STATS_INTERVAL_SEC = 5 # default is 1 second
 
-products = [
-    '0PUK6V6EV0',
-    '1YMWWN1N4O',
-    '2ZYFJ3GM2N',
-    '66VCHSJNUP',
-    '6E92ZMYYFZ',
-    '9SIQT8TOJO',
-    'L9ECAV7KIM',
-    'LS4PSXUNUM',
-    'OLJCESPC7Z']
-
-def index(l):
-    l.client.get("/")
-
-def setCurrency(l):
-    currencies = ['EUR', 'USD', 'JPY', 'CAD']
-    l.client.post("/setCurrency",
-        {'currency_code': random.choice(currencies)})
-
-def browseProduct(l):
-    l.client.get("/product/" + random.choice(products))
-
-def viewCart(l):
-    l.client.get("/cart")
-
-def addToCart(l):
-    product = random.choice(products)
-    l.client.get("/product/" + product)
-    l.client.post("/cart", {
-        'product_id': product,
-        'quantity': random.choice([1,2,3,4,5,10])})
-
-def checkout(l):
-    addToCart(l)
-    l.client.post("/cart/checkout", {
-        'email': 'someone@example.com',
-        'street_address': '1600 Amphitheatre Parkway',
-        'zip_code': '94043',
-        'city': 'Mountain View',
-        'state': 'CA',
-        'country': 'United States',
-        'credit_card_number': '4432-8015-6152-0454',
-        'credit_card_expiration_month': '1',
-        'credit_card_expiration_year': '2039',
-        'credit_card_cvv': '672',
-    })
-
 class UserBehavior(TaskSet):
 
-    def on_start(self):
-        index(self)
+    #def on_start(self):
+    #    index(self)
 
-    tasks = {index: 1,
-        setCurrency: 2,
-        browseProduct: 10,
-        addToCart: 2,
-        viewCart: 3,
-        checkout: 1}
+    @task(1)
+    def index(self):
+        self.client.get("/")
 
-class WebsiteUser(HttpUser):
+    @task(2)
+    def setCurrency(self):
+        currencies = ['EUR', 'USD', 'JPY', 'CAD']
+        self.client.post("/setCurrency",
+            {'currency_code': random.choice(currencies)})
+
+    @task(10)
+    def browseProduct(self):
+        products = [
+            '0PUK6V6EV0',
+            '1YMWWN1N4O',
+            '2ZYFJ3GM2N',
+            '66VCHSJNUP',
+            '6E92ZMYYFZ',
+            '9SIQT8TOJO',
+            'L9ECAV7KIM',
+            'LS4PSXUNUM',
+            'OLJCESPC7Z']
+
+        self.client.get("/product/" + random.choice(products))
+
+
+    @task(3)
+    def viewCart(self):
+        self.client.get("/cart")
+
+    @task(2)
+    def addToCart(self):
+        products = [
+            '0PUK6V6EV0',
+            '1YMWWN1N4O',
+            '2ZYFJ3GM2N',
+            '66VCHSJNUP',
+            '6E92ZMYYFZ',
+            '9SIQT8TOJO',
+            'L9ECAV7KIM',
+            'LS4PSXUNUM',
+            'OLJCESPC7Z']
+
+        product = random.choice(products)
+        self.client.get("/product/" + product)
+
+        self.client.post("/cart", {
+            'product_id': product,
+            'quantity': random.choice([1,2,3,4,5,10])})
+
+
+    @task(1)
+    def checkout(self):
+        products = [
+            '0PUK6V6EV0',
+            '1YMWWN1N4O',
+            '2ZYFJ3GM2N',
+            '66VCHSJNUP',
+            '6E92ZMYYFZ',
+            '9SIQT8TOJO',
+            'L9ECAV7KIM',
+            'LS4PSXUNUM',
+            'OLJCESPC7Z']
+
+        product = random.choice(products)
+        self.client.get("/product/" + product)
+
+        
+        self.client.post("/cart", {
+            'product_id': product,
+            'quantity': random.choice([1,2,3,4,5,10])})
+
+        self.client.post("/cart/checkout", {
+            'email': 'someone@example.com',
+            'street_address': '1600 Amphitheatre Parkway',
+            'zip_code': '94043',
+            'city': 'Mountain View',
+            'state': 'CA',
+            'country': 'United States',
+            'credit_card_number': '4432-8015-6152-0454',
+            'credit_card_expiration_month': '1',
+            'credit_card_expiration_year': '2039',
+            'credit_card_cvv': '672',
+        })
+            
+
+
+class WebsiteUser(FastHttpUser):
     tasks = [UserBehavior]
     wait_time = between(1, 10)
+
+    connection_timeout = 5.0
+    network_timeout = 5.0
 
 class CustomLoadShape(LoadTestShape):
 
